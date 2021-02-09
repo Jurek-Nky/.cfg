b920853132746b7d3eb197f8876a367957942f39
Jurek Nitschky <jurek.nit@gmail.com>
2021-02-09 09:56:48 +0100

    fish.config first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+fish_vi_key_bindings
+set -g theme_date_format "+%a %H:%M"
+set -g theme_date_timezone Germany/Berlin
+set -g theme_title_display_process yes
+set -g theme_display_user yes
+set -g theme_color_scheme solarized 
