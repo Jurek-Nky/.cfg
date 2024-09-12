83f95c776a98110472961f8978b03a5ae1a9cbed
Jurek Nitschky <Jurek.Nitschky@Vanderlande.com>
2024-09-12 11:48:05 +0200

    init lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
+vim.keymap.set("n", "qq", ":wq<CR>")
+vim.keymap.set("n", "fq", ":q!<CR>")
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
